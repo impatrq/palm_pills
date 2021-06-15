1985f602b6828f5d108eede508c4864bbd2ef6d4
MatiasOteroYGarzon <82232190+MatiasOteroYGarzon@users.noreply.github.com>
2021-06-15 16:12:30 -0300

    refactor: mover prototipos al .h

---
+++ software/pastillero/modulo_de_voz.h
@@ -0,0 +1,10 @@
+void write_Comand(unsigned char *);
+void dfplayer_pista(unsigned char);
+void dfplayer_Pause(void);
+void dfplayer_Play(void);
+void dfplayer_Next(void);
+void dfplayer_Prev(void);
+void dfplayer_Volume(unsigned char);
+void dfplayer_Volume_up(void);
+void dfplayer_Volume_down(void);
+void reproduccion_pista(int, int);
