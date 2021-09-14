ec7eb9890953d9df0ac78889dad448ae6dd7a9da
AugustoPagnoni <82232226+AugustoPagnoni@users.noreply.github.com>
2021-09-13 21:35:26 -0300

    refactor: se arreglo codigo y pines del display

--- software/pastillero/main.c
+++ software/pastillero/main.c
@@ -1,12 +1,3 @@
-/*Se definen los puertos del display*/
-#define LCD_RS_PIN      PIN_D0
-#define LCD_RW_PIN      PIN_D1
-#define LCD_ENABLE_PIN  PIN_D2
-#define LCD_DATA4       PIN_D3
-#define LCD_DATA5       PIN_D4
-#define LCD_DATA6       PIN_D5
-#define LCD_DATA7       PIN_D6
-//End LCD module connections
 
 #include <16F887.h>                      //incluimos el pic que vamos a usar
 #fuses HS,NOWDT,NOPROTECT,NOLVP,PUT                       
